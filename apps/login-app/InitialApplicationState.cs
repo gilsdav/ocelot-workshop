9659bec269a6a858fa58d8be20d777663de6e83b
David <davgilson@live.fr>
2020-05-26 17:05:38 +0200

    save id_token

--- apps/login-app/InitialApplicationState.cs
+++ apps/login-app/InitialApplicationState.cs
@@ -1,5 +1,6 @@
 public class InitialApplicationState
 {
     public string AccessToken { get; set; }
+    public string IdToken { get; set; }
     // public string RefreshToken { get; set; }
 }
--- apps/login-app/TokenProvider.cs
+++ apps/login-app/TokenProvider.cs
@@ -1,5 +1,6 @@
 public class TokenProvider
 {
     public string AccessToken { get; set; }
+    public string IdToken { get; set; }
     // public string RefreshToken { get; set; }
 }
